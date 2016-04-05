64bd427362cbeb1594a79eca79bc48c7e6de0a65
Samhith G B <gbsamhith@gmail.com>
2016-04-05 22:06:08 +0530

    Update connect.php

--- admin/connect.php
+++ admin/connect.php
@@ -1,4 +1,5 @@
 <?php
-$con=mysql_connect("localhost","root","root")or die("Something went wrong");
+$con=mysql_connect("localhost","user_name","password")or die("Something went wrong");
 mysql_select_db("Question_Paper_Generator",$con);
 ?>
+
